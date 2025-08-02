f797bb7e8c99682ac91d1aec02783d140f45ae05
Vel1Zy <adriel.ichwan@gmail.com>
2025-08-03 00:51:07 +0700

    better solution on problem 9.

--- 9-palindromeNumber.js
+++ 9-palindromeNumber.js
@@ -5,22 +5,44 @@
 
 //Given an integer x, return true if x is a palindrome, and false otherwise.
 
+// solution 3 - Optimization of solution 2
+var isPalindrome = function (x) {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+  let reversed = 0;
+  while (x > reversed) {
+    reversed = reversed * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  return x === reversed || x === Math.floor(reversed / 10);
+};
+
+var isPalindrome = function (x){
+  if(x<0||x%10 === 0 && x !== 0) return false;
+  let reversed = 0;
+  while(x> reversed){
+    reversed = reversed * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  return x === reversed || x === Math.floor(reversed / 10);
+
+}
+
 //solution 2 - try without converting it to string
 var isPalindrome = function (x) {
   let w = 0;
+  if(x==0) return false;
+  if(x%10 == 0) return false;
   while (x > w) {
     let s = x % 10;
 
     w = w * 10 + s;
     x = Math.floor(x / 10);
   }
+  
   if (x == w || x == Math.floor(w / 10)) return true;
   return false;
 };
-if (s === 0) return false;
-else {
-  reversedNumber = reversedNumber * 10 + s;
-}
 
 // solution 1 - easy way, converting it to string
 var isPalindrome = function (x) {
