a7d49d9e64230363c3ee385980da40d75e896c2b
Vel1Zy <adriel.ichwan@gmail.com>
2025-08-05 07:16:20 +0700

    problem 26-done

--- 0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array.js
@@ -2,6 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
+// sol 2 -> Optimization -> without rewriting the whole array (Because in the problem it is not required to change the input array into a specific)
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+  let count = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[count] = nums[i + 1];
+      count++;
+    }
+  }
+  return count;
+};
+
+//sol 1 -> too inefficient the O(n^2) sol are badd
 var removeDuplicates = function (nums) {
   if (nums.length === 0) return 0;
   let count = nums.length;
